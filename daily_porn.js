5fba657f12293b43a2dfb80d97f441968f9ad8db
SkyLeech <43989611+SkyLeech@users.noreply.github.com>
2025-10-22 16:46:11 +0800

    Refactor request data structure in daily_porn.js

--- daily_porn.js
+++ daily_porn.js
@@ -6,17 +6,18 @@
 
 const url = "https://api.lolicon.app/setu/v2?"
 
-const data = {
+const req = {
     "userR18": 1, //18禁为1 非为0 2是混合
     "userNum": 1, //一次返回的结果数量，范围为1到10，数字的数量亦为弹框的次数
     "size": ["original", "thumb"], //返回指定图片规格的地址
     "excludeAI": false //排除 AI 作品
 }
-let request = {
+
+const request = {
     url,
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
+    body: JSON.stringify(req)
 }
 $task.fetch(request).then(response => {
     let obj = JSON.parse(response.body);
